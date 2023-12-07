e87c70d36425cff3c90770aafe79fd437287e30c
nutthidapops <138448847+nutthidapops@users.noreply.github.com>
2023-12-07 15:31:22 +0700

    delete consloe

--- src/app/api-caller/create-Driver.ts
+++ src/app/api-caller/create-Driver.ts
@@ -4,9 +4,7 @@ import { log } from 'console'
 
 export const createDrivers = async (data: ICreateDriver) => {
   try {
-    console.log(data)
     const result = await axios.post('http://localhost:3001/driver', data)
-    console.log(result.data)
     return result.data
   } catch (error) {
     console.log(error)
