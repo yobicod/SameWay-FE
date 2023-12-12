7d481d381c76f13045765a17708222fa2aff9174
Kongooii <konglm41@gmail.com>
2023-12-12 20:30:05 +0700

    fix: remove func

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,8 @@
-import { getServerSession } from 'next-auth'
 import UserInfo from './(components)/UserInfo'
-import { getAllDrivers } from '../api-caller/get-all-drivers'
-import { createDriver } from '../api-caller/create-driver'
+
 import Link from 'next/link'
 
 export default async function HomePage() {
-  console.log(await createDriver())
   return (
     <div>
       <UserInfo />
