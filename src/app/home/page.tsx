1b07d328edf59c8bf4730381c4de952662562c56
Kongooii <konglm41@gmail.com>
2023-12-14 00:40:08 +0700

    feat: add input icon

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,12 +3,14 @@ import UserInfo from './(components)/UserInfo'
 
 import Link from 'next/link'
 import Button from '@/components/Button'
+import Input from '@/components/Input'
 
 export default async function HomePage() {
   return (
     <div>
       <UserInfo />
       <Link href='/driver'>driver</Link>
+      <Input endIcon={<Icon name='home' />} />
     </div>
   )
 }
