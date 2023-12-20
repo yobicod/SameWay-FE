3a8359e056a71d06811ae8d4e49a1f3945a60164
nutthidapops <138448847+nutthidapops@users.noreply.github.com>
2023-12-20 16:43:39 +0700

    feat: history

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -15,7 +15,7 @@ export default function FeedBackPage() {
           className="inset-x-0  rounded-t-[50px] flex flex-col gap-4 items-center"
           style={{ boxShadow: '0px -4px 4px 0px rgba(164, 159, 159, 0.25)' }}
         >
-          <div className="text-label font-bold flex justify-center items-center gap-1 h-20 w-screen  border-b-2 border-stroke border-opacity-20 relative">
+          <div className="text-label font-bold flex justify-center items-center gap-1 h-20 w-screen border-b-2 border-stroke border-opacity-20 relative">
             <BackButton pageRoute="/driver" />
             <p className="text-2xl  text-center font-lexendExa text-secondary">
               History
