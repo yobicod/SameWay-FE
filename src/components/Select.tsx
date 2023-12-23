480af4aa2a6cc11478dfe1d8d1ccda88320e3840
Kongooii <konglm41@gmail.com>
2023-12-23 20:19:59 +0700

    feat: implement select item

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -16,7 +16,7 @@ export default function SelectDemo({
 }: IProps) {
   return (
     <Select.Root value={selectedItem} onValueChange={onChange}>
-      <Select.Trigger className='border px-4 py-2 flex justify-between items-center rounded'>
+      <Select.Trigger className='border px-4 py-2 flex justify-between items-center rounded text-secondary border-stroke'>
         <Select.Value />
         <Select.Icon />
       </Select.Trigger>
@@ -25,11 +25,18 @@ export default function SelectDemo({
         <Select.Content
           position='popper'
           align='center'
-          className='border SelectContent p-2 rounded bg-white'>
+          className='border SelectContent p-2 rounded bg-white z-50'>
           <Select.ScrollUpButton />
           <Select.Viewport className='flex flex-col gap-1'>
-            {/* {items.map((item) =>)} */}
-            <Select.Item
+            {items.map((item) => (
+              <Select.Item
+                key={`select-item-${item}`}
+                value={item}
+                className='hover:bg-secondary hover:text-white px-2 py-1 rounded cursor-pointer'>
+                <Select.ItemText>{item}</Select.ItemText>
+              </Select.Item>
+            ))}
+            {/* <Select.Item
               value='test1'
               className='hover:bg-secondary hover:text-white px-2 py-1 rounded cursor-pointer'>
               <Select.ItemText>test1</Select.ItemText>
@@ -39,7 +46,7 @@ export default function SelectDemo({
               value='test2'
               className='hover:bg-secondary hover:text-white px-2 py-1 rounded cursor-pointer'>
               <Select.ItemText>test2</Select.ItemText>
-            </Select.Item>
+            </Select.Item> */}
 
             <Select.Separator />
           </Select.Viewport>
