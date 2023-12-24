6e233c61b1fcf87bd81d4da6eef7b7f1a70d4394
yobicod <yobicod.4u@gmail.com>
2023-12-24 21:35:43 +0700

    fix: wrong selected dropdown behavior

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import * as Select from '@radix-ui/react-select'
-import Icon from './Icon'
+import React, { useState } from 'react';
+import * as Select from '@radix-ui/react-select';
+import Icon from './Icon';
 
 interface IProps {
-  items: string[]
-  placeholder?: string
-  selectedItem: string
-  onChange: (value: string) => void
+  items: string[];
+  placeholder?: string;
+  selectedItem: string;
+  onChange: (value: string) => void;
 }
 export default function SelectDemo({
   items,
@@ -17,7 +17,7 @@ export default function SelectDemo({
   return (
     <Select.Root value={selectedItem} onValueChange={onChange}>
       <Select.Trigger className='border px-4 py-2 flex justify-between items-center rounded text-secondary border-stroke'>
-        <Select.Value placeholder={placeholder}/>
+        <Select.Value placeholder={placeholder} />
         <Select.Icon />
       </Select.Trigger>
 
@@ -25,14 +25,16 @@ export default function SelectDemo({
         <Select.Content
           position='popper'
           align='center'
-          className='border SelectContent p-2 rounded bg-white z-50'>
+          className='border selectContent p-2 rounded bg-white z-50'
+        >
           <Select.ScrollUpButton />
           <Select.Viewport className='flex flex-col gap-1'>
             {items.map((item) => (
               <Select.Item
                 key={`select-item-${item}`}
                 value={item}
-                className='hover:bg-secondary hover:text-white px-2 py-1 rounded cursor-pointer outline-none'>
+                className='hover:bg-secondary hover:text-white px-2 py-1 rounded cursor-pointer outline-none'
+              >
                 <Select.ItemText>{item}</Select.ItemText>
               </Select.Item>
             ))}
@@ -55,5 +57,5 @@ export default function SelectDemo({
         </Select.Content>
       </Select.Portal>
     </Select.Root>
-  )
+  );
 }
