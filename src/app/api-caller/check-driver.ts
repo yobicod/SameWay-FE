5e5700e4b221c7153016268d43878d0170372fe4
yobicod <yobicod.4u@gmail.com>
2023-12-24 23:28:03 +0700

    remove log

--- src/app/api-caller/check-driver.ts
+++ src/app/api-caller/check-driver.ts
@@ -1,6 +1,5 @@
 import axiosClient from '../lib/axiosClient';
 export const checkDriver = async (email: string) => {
-  console.log('🚀 ~ file: check-driver.ts:3 ~ checkDriver ~ email:', email);
   try {
     const { data } = await axiosClient.get<boolean>(
       `${process.env.NEXT_PUBLIC_API_URL}/driver/is-driver-in-system/${email}`
