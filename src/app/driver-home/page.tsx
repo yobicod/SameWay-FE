155cabafc36a84d91313d55e6669768aa5654c25
yobicod <yobicod.4u@gmail.com>
2023-12-27 12:19:58 +0700

    feat: implement socket connection
    emit and listening to event from server

--- src/app/driver-home/page.tsx
+++ src/app/driver-home/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect } from 'react';
 import DriverInfo from './(components)/DriverInfo';
 import NavbarDriver from '../(components)/NavbarDriver';
 
