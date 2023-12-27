606c58ffb640af475f1cddc3a90c22acf6c04faf
paifuiii <63070014@kmitl.ac.th>
2023-12-28 02:23:53 +0700

    feat : driverInformation Page :art:

---
+++ src/app/driver-information/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import React from "react";
+import DriverInfor from "./(component)/DriverInformation";
+
+export default function DriverInformation() {
+  return (
+    <div className="flex flex-col justify-between h-screen w-full">
+      <div className="absolute w-full h-full -z-10">
+        <Image
+          src="/imgFoundDriver/bg_foundDriver.svg"
+          width={0}
+          height={0}
+          alt="app-logo"
+          className="w-full object-cover h-full"
+        />
+      </div>
+      <DriverInfor />
+    </div>
+  );
+}
--- src/app/found-driver/page.tsx
+++ src/app/found-driver/page.tsx
@@ -14,6 +14,7 @@ export default async function FoundDriverPage() {
           alt='app-logo'
           className='w-full object-cover h-full'
         />
+        
       </div>
       <FoundDriver />
     </div>
