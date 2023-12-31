ad85ccc745fc6cbbd152935f737b976ea926ba19
Kongooii <konglm41@gmail.com>
2023-12-31 19:18:34 +0700

    feat: implement new longdo map component

---
+++ src/longdo/LongdoDemo.js
@@ -0,0 +1,77 @@
+'use client'
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import React, { useEffect, useState } from 'react'
+
+export default function LongdoDemo({ value, onChange }) {
+  let suggest
+  let search
+
+  const [mounted, setMounted] = useState(false)
+  const [longdoMap, setLongdoMap] = useState()
+  const [query, setQuery] = useState('')
+  const [result, setResult] = useState([])
+  const [locations, setLocations] = useState(value)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  useEffect(() => {
+    if (!longdoMap && mounted) {
+      initMap()
+    }
+  }, [longdoMap, mounted])
+  function initMap() {
+    suggest = document.getElementById('suggest')
+    const newMap = new window.longdo.Map({
+      placeholder: document.getElementById('map'),
+      layer: [longdo.Layers.GRAY, longdo.Layers.TRAFFIC],
+      ui: longdo.UiComponent.Mobile,
+      lastView: false,
+      language: 'th',
+      placeholderHtml: '<p>Loading map...</p>',
+      zoom: 15,
+    })
+    setTimeout(() => {
+      if (value) {
+        newMap.Route.add(new longdo.Marker(locations[0]))
+        newMap.Route.add(new longdo.Marker(locations[1]))
+        newMap.Route.search()
+      }
+    }, 1500)
+    console.log('start')
+    setLongdoMap(newMap)
+  }
+  async function querySearch() {
+    longdoMap.Search.placeholder(document.getElementById('result'))
+    const searchResult = await longdoMap.Search.search(query)
+    setResult(searchResult.data)
+  }
+  function addRoute() {
+    let mouseLocation = longdoMap.location(longdo.LocationMode.Pointer)
+    const marker = new longdo.Marker(mouseLocation)
+    if (locations.length === 2) {
+      longdoMap.Route.removeAt(1)
+      // remove lastest location
+      const clonedLocation = locations.splice(1, 1)
+      onChange(clonedLocation)
+      setLocations(clonedLocation)
+    } else {
+      const clonedLocation = [...locations, mouseLocation]
+      longdoMap.Route.add(marker)
+      setLocations(clonedLocation)
+      onChange(clonedLocation)
+    }
+
+    // onChange(console.log(locations))
+    longdoMap.Route.placeholder(document.getElementById('result'))
+  }
+
+  return (
+    <div className='w-full'>
+      <div id='map' style={{ height: 500 }} onClick={addRoute} />
+      <div id='result' />
+      {/* <Input onChange={(e) => setQuery(e.target.value)} />
+      <Button onClick={() => querySearch()}>test</Button> */}
+    </div>
+  )
+}
