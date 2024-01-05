abdeb4e8bfd70d1eb0f9218e99c77b50a8d5b84b
Kongooii <konglm41@gmail.com>
2024-01-06 04:12:21 +0700

    feat: longdo map v.3

---
+++ src/longdo/MapTest.tsx
@@ -0,0 +1,101 @@
+'use client'
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import React, { useEffect, useRef, useState } from 'react'
+
+interface IGeoLatLon {
+  lat: number
+  lon: number
+}
+
+interface IProps {
+  value?: IGeoLatLon | undefined
+  onChange?: () => void
+  height?: string
+  disabled?: boolean
+  callback?: () => void
+}
+export let longdo
+export let map
+export default function MapTest({
+  value,
+  onChange,
+  height = '500px',
+  disabled = false,
+  callback,
+}: IProps) {
+  const [mounted, setMounted] = useState(false)
+  const mapRef = useRef(null)
+  // const [mapCallback, setMapCallback] = useState(() => {
+  //   return () => {
+  //     longdo = window.longdo
+  //     map = new window.longdo.Map({
+  //       placeholder: document.getElementById('map'),
+  //       layer: [longdo.Layers.GRAY, longdo.Layers.TRAFFIC],
+  //       ui: longdo.UiComponent.Mobile,
+  //       lastView: false,
+  //       language: 'th',
+  //       placeholderHtml: '<p>Loading map...</p>',
+  //       zoom: 15,
+  //     })
+  //   }
+  // })
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  useEffect(() => {
+    if (mounted) {
+      mapCallback()
+      setTimeout(() => {
+        if (value) {
+          map.Overlays.add(new longdo.Marker(value))
+          map.location(value)
+        } else {
+          map.location(longdo.LocationMode.Geolocation)
+        }
+      }, 500)
+    }
+    if (mounted && callback) {
+      callback()
+    }
+  }, [mounted, callback, value])
+
+  const mapCallback = () => {
+    longdo = window.longdo
+    map = new window.longdo.Map({
+      placeholder: mapRef.current,
+      layer: [longdo.Layers.GRAY, longdo.Layers.TRAFFIC],
+      ui: longdo.UiComponent.Mobile,
+      lastView: false,
+      language: 'th',
+      placeholderHtml: '<p>Loading map...</p>',
+      zoom: 15,
+    })
+  }
+  function addRoute() {
+    let mouseLocation = map.location(longdo.LocationMode.Pointer)
+    const marker = new longdo.Marker(mouseLocation)
+
+    if (map.Overlays.list().length >= 1) {
+      console.log('more')
+      map.Overlays.clear()
+      if (onChange) onChange({})
+      // setLocations(undefined)
+    } else {
+      map.Overlays.add(marker, { draggable: true })
+      if (onChange) onChange(mouseLocation)
+      // setLocations(mouseLocation)
+    }
+    console.log(value)
+  }
+  return (
+    <>
+      <div
+        ref={mapRef}
+        id='map'
+        className='h-full w-full'
+        onClick={() => !disabled && addRoute()}
+      />
+    </>
+  )
+}
