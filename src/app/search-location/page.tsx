1806716b5e9a3a3433e4bdd78ed224f94bcdabc0
Kongooii <konglm41@gmail.com>
2024-01-06 16:10:22 +0700

    refactor: remove unnecessary tag

--- src/app/search-location/page.tsx
+++ src/app/search-location/page.tsx
@@ -1,12 +1,6 @@
-import Image from 'next/image'
 import React from 'react'
 import CurrentLocation from './(components)/CurrentLocation'
-import SearchLocation from './(components)/SelectLocation'
 
 export default function SearchLocationPage() {
-  return (
-    <div className='flex flex-col gap-4  relative h-screen'>
-      <CurrentLocation />
-    </div>
-  )
+  return <CurrentLocation />
 }
