5ef1b824a19549929cd51c5d19f75b974fb1f2b3
yobicod <yobicod.4u@gmail.com>
2024-01-07 20:49:58 +0700

    fix: socket connection 1 on 1

--- src/app/select-location/(components)/CurrentLocation.tsx
+++ src/app/select-location/(components)/CurrentLocation.tsx
@@ -1,77 +1,56 @@
-'use client'
-import Button from '@/components/Button'
-import Input from '@/components/Input'
-import Icon from '@/components/Icon'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Controller, useForm } from 'react-hook-form'
-import { z } from 'zod'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import { debounce } from 'lodash'
-import MapTest, { map } from '@/longdo/MapTest'
+'use client';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import Icon from '@/components/Icon';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+import MapTest, { map } from '@/longdo/MapTest';
 
-import { io } from 'socket.io-client'
+import { io } from 'socket.io-client';
 import {
   queryLocation,
   queryLocationByGeoLocation,
   querySuggestLocation,
-} from '../(api)/getLongDoApi'
+} from '../(api)/getLongDoApi';
 interface IGeoLatLon {
-  lat: number
-  lon: number
+  lat: number;
+  lon: number;
 }
-type Map = 'start' | 'end' | undefined
+type Map = 'start' | 'end' | undefined;
 export default function CurrentLocation() {
-  const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`, {
-    transports: ['websocket'],
-  })
+  const [driverList, setDriverList] = useState<any>();
+  const [socket, setSocket] = useState<any>(null);
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('Connect to server successful✅')
-    })
+    const newSocket = io(`${process.env.NEXT_PUBLIC_API_URL}`, {
+      transports: ['websocket'],
+    });
 
-    socket.on('driverStatus', (data) => {
-      console.log('Data from server => ', data)
-    })
+    newSocket.on('connect', () => {
+      console.log('Connect to server successful✅');
+    });
 
-    socket.on('waitingForDriver', (data: any) => {
-      console.log(data)
-    })
+    newSocket.on('waitingForDriver', (data) => {
+      console.log('Waiting for driver:', data);
+    });
 
-    //  socket.emit('waitForPassenger', {
-    //    fullName: 'Visal Suwanarat',
-    //    driverEmail: '63070158@kmitl.ac.th',
-    //    driverLat: 10.0,
-    //    driverLong: 10.0,
-    //    time: 12.0,
-    //    status: 'open',
-    //    capacity: 4,
-    //    carType: 'car',
-    //    plate: 'aasd213',
-    //    sex: 'Male',
-    //    phoneNumber: '0658386230',
-    //  })
+    setSocket(newSocket);
 
-    //  socket.emit('findDriver', {
-    //    fullName: 'Visal Suwanarat',
-    //    driverEmail: '63070158@kmitl.ac.th',
-    //    driverLat: 10.0,
-    //    driverLong: 10.0,
-    //    time: 12.0,
-    //    status: 'open',
-    //    capacity: 4,
-    //    carType: 'car',
-    //    plate: 'aasd213',
-    //    sex: 'Male',
-    //    phoneNumber: '0658386230',
-    //  })
-  }, [])
-  const [showMap, setShowMap] = useState<Map>()
-  const [showSuggestion, setShowSuggestion] = useState(false)
-  const [search, setSearch] = useState('')
-  const [suggestLocation, setSuggestLocation] = useState([])
-  const [startLocationDetail, setStartLocationDetail] = useState('')
-  const [endLocationDetail, setEndLocationDetail] = useState('')
+    return () => {
+      if (newSocket) {
+        newSocket.disconnect();
+      }
+    };
+  }, []);
+  const [showMap, setShowMap] = useState<Map>();
+  const [showSuggestion, setShowSuggestion] = useState(false);
+  const [search, setSearch] = useState('');
+  const [suggestLocation, setSuggestLocation] = useState([]);
+  const [startLocationDetail, setStartLocationDetail] = useState('');
+  const [endLocationDetail, setEndLocationDetail] = useState('');
   const searchDriverSchema = z.object({
     locationStart: z.object({
       lon: z.number({
@@ -94,9 +73,9 @@ export default function CurrentLocation() {
       }),
     }),
     notes: z.string(),
-  })
+  });
 
-  type searchDriverData = z.infer<typeof searchDriverSchema>
+  type searchDriverData = z.infer<typeof searchDriverSchema>;
   const {
     handleSubmit,
     control,
@@ -111,39 +90,39 @@ export default function CurrentLocation() {
       locationEnd: {},
       notes: '',
     },
-  })
+  });
 
   function onInputChange(keyword: string) {
-    setSearch(keyword)
-    debounceHandleSearchLocation(keyword)
+    setSearch(keyword);
+    debounceHandleSearchLocation(keyword);
   }
-  const watchStartLocation = watch('locationStart')
-  const watchEndLocation = watch('locationEnd')
+  const watchStartLocation = watch('locationStart');
+  const watchEndLocation = watch('locationEnd');
   const mapCallback = () => {
     setTimeout(() => {
       if (Object.keys(watchStartLocation).length > 0) {
-        map.Route.add(new longdo.Marker(watchStartLocation))
-        map.location(watchStartLocation)
+        map.Route.add(new longdo.Marker(watchStartLocation));
+        map.location(watchStartLocation);
       }
       if (Object.keys(watchEndLocation).length > 0) {
-        map.Route.add(new longdo.Marker(watchEndLocation))
+        map.Route.add(new longdo.Marker(watchEndLocation));
       }
       if (
         Object.keys(watchStartLocation).length > 0 &&
         Object.keys(watchEndLocation).length > 0
       ) {
-        map.Route.search()
+        map.Route.search();
       }
-    }, 500)
-  }
+    }, 500);
+  };
   const getSuggestLocation = async (keyword: string) => {
-    const result = await querySuggestLocation(keyword)
-    setSuggestLocation(result?.data?.data || [])
-  }
+    const result = await querySuggestLocation(keyword);
+    setSuggestLocation(result?.data?.data || []);
+  };
   const selectSuggestLocationStart = async (keyword: string) => {
-    const result = await queryLocation(keyword)
-    setSearch(keyword)
-    setStartLocationDetail(result.data.data[0].name || 'จุดเริ่มต้น')
+    const result = await queryLocation(keyword);
+    setSearch(keyword);
+    setStartLocationDetail(result.data.data[0].name || 'จุดเริ่มต้น');
     setValue(
       'locationStart',
       {
@@ -151,12 +130,12 @@ export default function CurrentLocation() {
         lon: result.data.data[0].lon,
       },
       { shouldValidate: true }
-    )
-  }
+    );
+  };
   const selectSuggestLocationEnd = async (keyword: string) => {
-    const result = await queryLocation(keyword)
-    setSearch(keyword)
-    setEndLocationDetail(result.data.data[0].name || 'จุดหมายปลายทาง')
+    const result = await queryLocation(keyword);
+    setSearch(keyword);
+    setEndLocationDetail(result.data.data[0].name || 'จุดหมายปลายทาง');
     setValue(
       'locationEnd',
       {
@@ -164,11 +143,11 @@ export default function CurrentLocation() {
         lon: result.data.data[0].lon,
       },
       { shouldValidate: true }
-    )
-  }
+    );
+  };
   const handleOnClickStartMap = async (geoLocation: IGeoLatLon) => {
-    const result = await queryLocationByGeoLocation(geoLocation)
-    const data = result.data
+    const result = await queryLocationByGeoLocation(geoLocation);
+    const data = result.data;
     const detail = [
       data?.aoi,
       data?.road,
@@ -176,12 +155,12 @@ export default function CurrentLocation() {
       data?.subdistrict,
       data?.province,
       data?.postcode,
-    ]
-    setStartLocationDetail(detail.join(' '))
-  }
+    ];
+    setStartLocationDetail(detail.join(' '));
+  };
   const handleOnClickEndMap = async (geoLocation: IGeoLatLon) => {
-    const result = await queryLocationByGeoLocation(geoLocation)
-    const data = result.data
+    const result = await queryLocationByGeoLocation(geoLocation);
+    const data = result.data;
     const detail = [
       data?.aoi,
       data?.road,
@@ -189,37 +168,40 @@ export default function CurrentLocation() {
       data?.subdistrict,
       data?.province,
       data?.postcode,
-    ]
+    ];
 
-    setEndLocationDetail(detail.join(' '))
-  }
-  const debounceHandleSearchLocation = debounce(getSuggestLocation, 700)
+    setEndLocationDetail(detail.join(' '));
+  };
+  const debounceHandleSearchLocation = debounce(getSuggestLocation, 700);
   const handleSwap = () => {
     if (startLocationDetail && endLocationDetail) {
-      const clonedStartLocation = watchStartLocation
-      const clonedStartDetail = startLocationDetail
-      setValue('locationStart', watchEndLocation)
-      setValue('locationEnd', clonedStartLocation)
-      setStartLocationDetail(endLocationDetail)
-      setEndLocationDetail(clonedStartDetail)
+      const clonedStartLocation = watchStartLocation;
+      const clonedStartDetail = startLocationDetail;
+      setValue('locationStart', watchEndLocation);
+      setValue('locationEnd', clonedStartLocation);
+      setStartLocationDetail(endLocationDetail);
+      setEndLocationDetail(clonedStartDetail);
     }
-  }
+  };
   const submitForm = async (data: searchDriverData) => {
-    console.log('daw', data)
-    socket.emit('findDriver', {
-      fullName: 'Athichart Chusri',
-      driverEmail: '63070181@kmitl.ac.th',
-      driverLat: data.locationStart.lat,
-      driverLong: data.locationStart.lon,
-      time: 12.0,
-      status: 'open',
-      capacity: 4,
-      carType: 'car',
-      plate: 'aasd213',
-      sex: 'Male',
-      phoneNumber: '0658386230',
-    })
-  }
+    if (socket) {
+      console.log('Emit -> ', data);
+      socket.emit('findDriver', {
+        fullName: 'Athichart Chusri',
+        driverEmail: '63070181@kmitl.ac.th',
+        driverLat: data.locationStart.lat,
+        driverLong: data.locationStart.lon,
+        time: 12.0,
+        status: 'open',
+        capacity: 4,
+        carType: 'car',
+        plate: 'aasd213',
+        sex: 'Male',
+        phoneNumber: '0658386230',
+      });
+    }
+  };
+
   if (showMap === 'start') {
     return (
       <div className='h-screen relative'>
@@ -231,8 +213,8 @@ export default function CurrentLocation() {
             <MapTest
               height='h-full'
               onChange={(geoLocation) => {
-                onChange(geoLocation)
-                handleOnClickStartMap(geoLocation)
+                onChange(geoLocation);
+                handleOnClickStartMap(geoLocation);
               }}
               value={value}
             />
@@ -254,9 +236,10 @@ export default function CurrentLocation() {
                     key={`location-${location.w}`}
                     className='p-1 rounded hover:text-white hover:bg-secondary cursor-pointer'
                     onClick={() => {
-                      selectSuggestLocationStart(location.w)
-                      setShowSuggestion(false)
-                    }}>
+                      selectSuggestLocationStart(location.w);
+                      setShowSuggestion(false);
+                    }}
+                  >
                     {location?.w}
                   </div>
                 ))}
@@ -268,14 +251,15 @@ export default function CurrentLocation() {
           <div>
             <Button
               onClick={() => {
-                setShowMap(undefined)
-              }}>
+                setShowMap(undefined);
+              }}
+            >
               ยืนยัน
             </Button>
           </div>
         </div>
       </div>
-    )
+    );
   }
   if (showMap === 'end') {
     return (
@@ -288,8 +272,8 @@ export default function CurrentLocation() {
             <MapTest
               height='h-full'
               onChange={(geoLocation) => {
-                onChange(geoLocation)
-                handleOnClickEndMap(geoLocation)
+                onChange(geoLocation);
+                handleOnClickEndMap(geoLocation);
               }}
               value={value}
             />
@@ -311,9 +295,10 @@ export default function CurrentLocation() {
                     key={`location-${location.w}`}
                     className='p-1 rounded hover:text-white hover:bg-secondary cursor-pointer'
                     onClick={() => {
-                      selectSuggestLocationEnd(location.w)
-                      setShowSuggestion(false)
-                    }}>
+                      selectSuggestLocationEnd(location.w);
+                      setShowSuggestion(false);
+                    }}
+                  >
                     {location?.w}
                   </div>
                 ))}
@@ -325,14 +310,15 @@ export default function CurrentLocation() {
           <div>
             <Button
               onClick={() => {
-                setShowMap(undefined)
-              }}>
+                setShowMap(undefined);
+              }}
+            >
               ยืนยัน
             </Button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   return (
@@ -342,7 +328,8 @@ export default function CurrentLocation() {
       </div>
       <div
         className='rounded-t-5xl bg-white flex justify-center px-8 py-9 h-fit'
-        style={{ boxShadow: '0px -4px 4px 0px rgba(164, 159, 159, 0.25)' }}>
+        style={{ boxShadow: '0px -4px 4px 0px rgba(164, 159, 159, 0.25)' }}
+      >
         <div className='flex flex-col gap-5 w-80'>
           <p className='text-3xl text-secondary font-light'>
             วันนี้คุณปุยปุยอยาก <span className='font-medium'>ไปที่ไหน ?</span>
@@ -371,7 +358,8 @@ export default function CurrentLocation() {
             <div className='w-full'>
               <div
                 onClick={() => setShowMap('start')}
-                className='flex gap-2 bg-white rounded-4xl h-[58px] items-center p-2 text-stroke'>
+                className='flex gap-2 bg-white rounded-4xl h-[58px] items-center p-2 text-stroke'
+              >
                 <Icon
                   name='location_on'
                   className='material-symbols-outlined text-fieldOrange md-30'
@@ -382,7 +370,7 @@ export default function CurrentLocation() {
               </div>
 
               {errors.locationStart && (
-                <p className='text-red-500 font-light text-sm px-4'>
+                <p className='text-red-500 font-light text-sm px-4 py-2'>
                   {errors.locationStart.lat?.message}
                 </p>
               )}
@@ -390,7 +378,8 @@ export default function CurrentLocation() {
             <div className='w-full'>
               <div
                 onClick={() => setShowMap('end')}
-                className='flex gap-2 bg-white rounded-4xl h-[58px] items-center p-2 text-stroke'>
+                className='flex gap-2 bg-white rounded-4xl h-[58px] items-center p-2 text-stroke'
+              >
                 <Icon
                   name='location_on'
                   className='material-symbols-outlined text-secondary md-30'
@@ -400,7 +389,7 @@ export default function CurrentLocation() {
                 </p>
               </div>
               {errors.locationEnd && (
-                <p className='text-red-500 font-light text-sm px-4'>
+                <p className='text-red-500 font-light text-sm px-4 py-2'>
                   {errors.locationEnd.lat?.message}
                 </p>
               )}
@@ -420,5 +409,5 @@ export default function CurrentLocation() {
         </div>
       </div>
     </div>
-  )
+  );
 }
